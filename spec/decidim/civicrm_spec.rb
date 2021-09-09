720854b77dfca6ef5c9da4f32014ca791694c8c8
Vera Rojman <vrojman@protonmail.com>
2021-09-09 15:34:42 +0200

    Remove default generic spec

--- spec/decidim/civicrm_spec.rb
+++ spec/decidim/civicrm_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Decidim::Civicrm do
   it "has a version number" do
     expect(Decidim::Civicrm.version).not_to be nil
   end
-
-  it "does something useful" do
-    expect(Decidim::Civicrm).to eq("Be useful")
-  end
 end
