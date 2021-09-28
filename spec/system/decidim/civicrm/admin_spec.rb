80665470602e0675be41af9f625a324950ec6590
Vera Rojman <vrojman@protonmail.com>
2021-09-28 12:16:49 +0200

    Add specs

--- spec/system/decidim/civicrm/admin_spec.rb
+++ spec/system/decidim/civicrm/admin_spec.rb
@@ -9,6 +9,9 @@ describe "Decidim CiViCRM Admin section", type: :system do
   let!(:groups) { create_list :decidim_civicrm_group, 3, organization: organization }
   let!(:membership_types) { create_list :decidim_civicrm_membership_type, 3, organization: organization }
 
+  let!(:contact) { create :decidim_civicrm_contact, organization: organization }
+  let!(:group_membership) { create :decidim_civicrm_group_membership, contact: contact, group: groups.first, organization: organization }
+
   before do
     switch_to_host(organization.host)
     sign_in user
@@ -41,6 +44,22 @@ describe "Decidim CiViCRM Admin section", type: :system do
         expect(page).to have_content(groups[0].title)
         expect(page).to have_content(groups[1].title)
         expect(page).to have_content(groups[2].title)
+      end
+    end
+  end
+
+  describe "Group members page" do
+    before do
+      visit decidim_civicrm_admin.group_path(groups.first)
+    end
+
+    it "loads the page" do
+      expect(page).to have_content("Group members")
+      expect(page).to have_content(groups.first.title)
+
+      within ".civicrm-group-members" do
+        expect(page).to have_content(contact.user.name)
+        expect(page).to have_content(contact.user.nickname)
       end
     end
   end
