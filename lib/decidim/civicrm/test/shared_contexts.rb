2f924d8c9da4709d6e96bb06f797f581b907253b
Ivan Vergés <ivan@platoniq.net>
2021-10-13 14:00:57 +0200

    fix request specs

---
+++ lib/decidim/civicrm/test/shared_contexts.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+shared_context "stubs example api" do
+  let(:url) { "https://api.example.org/" }
+  let(:http_method) { :get }
+  let(:http_status) { 200 }
+  let(:data) do
+    {
+      "values" => [],
+      "is_error" => 0,
+      "version" => 3,
+      "count" => 0
+    }
+  end
+  let(:params) do
+    {}
+  end
+
+  before do
+    allow(Decidim::Civicrm::Api).to receive(:url).and_return(url)
+    stub_request(http_method, /api\.example\.org/)
+      .to_return(status: http_status, body: data.to_json, headers: {})
+  end
+end
--- spec/decidim/civicrm_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Decidim::Civicrm do
-  it "has a version number" do
-    expect(Decidim::Civicrm.version).not_to be nil
-  end
-end
