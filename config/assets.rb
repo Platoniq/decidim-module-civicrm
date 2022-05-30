41fcc5ae74245db8d78d7622d3a33c2c685f5049
Ivan Vergés <ivan@platoniq.net>
2022-05-30 13:49:52 +0200

    mv foundation theme to classic webpacker handler (#25)

--- config/assets.rb
+++ config/assets.rb
@@ -21,7 +21,3 @@ Decidim::Webpacker.register_path("#{base_path}/app/packs")
 Decidim::Webpacker.register_entrypoints(
   decidim_admin_civicrm_selects: "#{base_path}/app/packs/entrypoints/decidim_admin_civicrm_selects.js"
 )
-
-# If you want to do the same but include the SCSS file for the admin panel's
-# main SCSS file, you can use the following method.
-Decidim::Webpacker.register_stylesheet_import("stylesheets/vendor/select2-foundation-theme.css", group: :admin)
