d890b38b41f5f4e79df02401775ece4cfb5bdd84
Fran Bolívar <francisco.bolivar.lupianez@gmail.com>
2023-05-16 12:58:27 +0200

    Fix AuthorizeUser call by passing missing argument (#30)
    * Fix AuthorizeUser call by passing missing argument
    
    * Update minimum required version to 0.26

--- lib/decidim/civicrm/version.rb
+++ lib/decidim/civicrm/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-civicrm version.
   module Civicrm
     DECIDIM_VERSION = "0.26.1"
-    COMPAT_DECIDIM_VERSION = ">= 0.25.0, < 0.27"
-    VERSION = "0.4"
+    COMPAT_DECIDIM_VERSION = ">= 0.26.1, < 0.27"
+    VERSION = "0.5"
   end
 end
