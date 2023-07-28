88a9b4532bc8980d9f2ae2a4290d8a3e02a04213
Marina Muñoz <marinamuca01@gmail.com>
2023-07-28 12:45:16 +0200

    Fix linter errors

--- lib/decidim/civicrm/admin_engine.rb
+++ lib/decidim/civicrm/admin_engine.rb
@@ -47,11 +47,11 @@ module Decidim
       initializer "decidim.civicrm.admin_menu" do
         Decidim.menu :admin_menu do |menu|
           menu.add_item I18n.t("menu.civicrm", scope: "decidim.admin", default: "CiViCRM"),
-                    decidim_civicrm_admin.groups_path,
-                    icon_name: "people",
-                    position: 5.75,
-                    active: is_active_link?(decidim_civicrm_admin.groups_path, :inclusive),
-                    if: defined?(current_user) && current_user&.read_attribute("admin")
+                        decidim_civicrm_admin.groups_path,
+                        icon_name: "people",
+                        position: 5.75,
+                        active: is_active_link?(decidim_civicrm_admin.groups_path, :inclusive),
+                        if: defined?(current_user) && current_user&.read_attribute("admin")
         end
       end
 
